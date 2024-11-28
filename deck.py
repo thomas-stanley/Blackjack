68bcc94ef40a0a81d3b4270f889aeefabb932d60
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2024-11-28 14:44:28 +0000

    Added shuffling and debugging tools

--- deck.py
+++ deck.py
@@ -1,7 +1,19 @@
 import cards
 class Deck:
     def __init__(self):
-        self.deck = cards.Card().card_setup()
+        self.shuffle()
     
     def deal(self):
         return self.deck.pop()
+    
+    def shuffle(self):
+        self.deck = cards.card_setup()
+
+
+def main():
+    test_deck = Deck()
+    print(test_deck.deal())
+
+
+if __name__ == "__main__":
+    main()
