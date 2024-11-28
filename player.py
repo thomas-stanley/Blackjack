c65b0721da226c38a59d8f241f9fc8711fd36751
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2024-11-28 15:03:48 +0000

    Set up player's hand and debugging

--- player.py
+++ player.py
@@ -3,6 +3,17 @@ class Player:
     def __init__(self):
         self.hand = []
     
-    def hand(self):
+    def deal_hand(self):
         self.hand.append(deck.Deck().deal())
         self.hand.append(deck.Deck().deal())
+        return self.hand
+
+
+def main():
+    test_player = Player()
+    print(test_player.deal_hand())
+    print(test_player.hand)
+
+
+if __name__ == "__main__":
+    main()
